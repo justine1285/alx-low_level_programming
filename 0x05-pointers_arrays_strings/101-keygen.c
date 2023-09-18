b3d9aab4434b537d015c5d7b54eec8651c1bf794
justine1285 <justineadugbo@gmail.com>
2023-09-18 12:03:52 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid password for the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+
+	return (0);
+}
