1e086912dc81322924518ec49ed2da7007d66792
justine1285 <justineadugbo@gmail.com>
2023-10-10 08:32:42 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,41 +9,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	size_t n, o, i;
+	unsigned int n1, o1, i;
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+		return (NULL);
+	for (n1 = 0; name[n1]; n1++)
+		;
+	n1++;
+	dog->name = malloc(n1 * sizeof(char));
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	for (n = 0; name[0] != '\0'; n++)
+	for (i = 0; i < n1; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (o1 = 0; owner[o1]; o1++)
 		;
-	n++;
-	dog->name = malloc(sizeof(char) * n);
-	if (dog->name == NULL)
+	o1++;
+	dog->owner = malloc(o1 * sizeof(char));
+	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < n; i++)
-		dog->name[i] = name[i];
-	dog->age = age;
-	for (o = 0; owner[o] != '\0'; o++)
-		;
-	o++;
-	dog->owner = malloc(sizeof(char) * o);
-	if (dog->owner == NULL)
-	{
-		free(dog->owner);
-		free(dog);
-		return (NULL);
-	}
-	for (i = 0; i < o; i++)
+	for (i = 0; i < o1; i++)
 		dog->owner[i] = owner[i];
 	return (dog);
 }
