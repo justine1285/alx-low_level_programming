756e638c62cb6143825257a6e0220360708349db
justine1285 <justineadugbo@gmail.com>
2023-12-22 01:27:43 -0800

    key -> index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - get the index at which a key/value
+ * pair should be stored in array of a hash table
+ * @key: the key to get the index
+ * @size: the size of the array
+ * Return: the index of the key
+ * Description: uses the djb2 algorithm.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
